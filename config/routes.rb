351cdf4aea981bf9e7834fb9631cff696f08bf00
Sairoden <odingandarosa@gmail.com>
2021-06-20 16:53:07 +0800

    Signup users from UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :comments
-  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "pages#home"
   get "about", to: "pages#about"
+  get "signup", to: "users#new"
+  resources :users, except: [:new]
 
   resources :articles
 end
