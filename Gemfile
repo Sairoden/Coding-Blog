4c0358ff97d86db360282497227a218fe43ef9c9
Sairoden <odingandarosa@gmail.com>
2021-06-22 17:50:03 +0800

    Add categories model and tests

--- Gemfile
+++ Gemfile
@@ -5,7 +5,7 @@ ruby '2.7.3'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails', branch: 'main'
 gem 'rails', '~> 6.1.3', '>= 6.1.3.2'
-
+gem 'rails-controller-testing', '~> 0.0.3'
 gem 'bcrypt', '~> 3.1.7'
 gem "will_paginate", "~> 3.1.7"
 gem 'will_paginate-bootstrap4'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_21_110948) do
+ActiveRecord::Schema.define(version: 2021_06_22_033842) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -20,12 +20,10 @@ ActiveRecord::Schema.define(version: 2021_06_21_110948) do
     t.integer "user_id"
   end
 
-  create_table "comments", force: :cascade do |t|
-    t.text "description"
-    t.integer "user_id", null: false
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -37,5 +35,4 @@ ActiveRecord::Schema.define(version: 2021_06_21_110948) do
     t.boolean "admin", default: false
   end
 
-  add_foreign_key "comments", "users"
 end
